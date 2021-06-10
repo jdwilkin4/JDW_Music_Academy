ab7ca34c60e0f4acf215659296f55a2787ad869a
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-09 22:12:14 -0700

    editing port variable in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 const app = express()
 
 app.get('/', (req, res) => {
