a3c242c166dafacd7bc76c44fa9982c2d7060604
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-10 23:44:51 -0700

    adding mongo uri

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const port = process.env.PORT || 8000
 const app = express()
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
