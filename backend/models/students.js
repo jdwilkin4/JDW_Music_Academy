76ca1e1e048738b93b3c095845f899ed80b94afc
jdwilkin4 <jwilkin4@hotmail.com>
2021-08-28 23:37:32 -0700

    updating schema for student.js

--- backend/models/students.js
+++ backend/models/students.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const { Schema } = mongoose
 
 const studentSchema = new Schema({
-    name: String,
-    age: Number,
-    grade: Number,
-    instrument: String
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32,
+        unique: true
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    age: { type: Number },
+    grade: { type: Number },
+    instrument: {
+        type: String,
+        required: true,
+    },
+    about_profile: {
+        type: String,
+        trim: true
+    },
+    role: {
+        type: Number,
+        default: 0
+    }
 })
 
 module.exports = mongoose.model('student', studentSchema)
